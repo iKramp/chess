c5430b4fd5c0a3c7d268616d37d0a195852b22d2
iKramp <60236692+iKramp@users.noreply.github.com>
2022-08-28 16:55:46 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+extern crate sdl2;
+
+fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem.window("pc sim", 400, 400)
+        .position_centered()
+        .build()
+        .unwrap();
+    let mut canvas = window.into_canvas().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();//code above inits sdl2 somehow, idk what it does
+
+    'running: loop{
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit { .. } |
+                sdl2::event::Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::Escape), .. } => {
+                    break 'running
+                }
+                _ => {}
+            }
+        }
+    }
+}
